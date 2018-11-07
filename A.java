7c238022a909acb302aa093a28c24d3374244b24
NS18304015 <ns18304015@ga.tera-house.ac.jp>
2018-11-07 15:15:15 +0900

    jjajfakdj;akl

---
+++ A.java
@@ -0,0 +1,34 @@
+class AAA{
+	protected String name;
+	protected int money;
+	public AAA(String name, int money){
+		this.name=name;
+		this.money=money;
+	}
+	public void show(){
+		System.out.println(name+"\t"+money);
+	}
+}
+class BBB extends AAA{
+	protected int a,c;
+	
+	public BBB(String name,int money,int _a){
+		super(name,money);
+		a=_a;
+	}
+	public void income(int _c){
+		c=_c;
+		System.out.println(name+"ÇÕ"+(money+c)+"â~ÇÃèäéùã‡Ç∆ÅA");
+	}
+	public void showIncome(){
+		System.out.println(a-c+"ÇÃëÃóÕÇ…Ç»ÇËÇ‹ÇµÇΩ");
+	}
+}
+class Test{
+	public static void main(String args[]){
+		BBB b=new BBB("suzuki",5000,8000);
+		b.show();
+		b.income(800);
+		b.showIncome();
+	}
+}
